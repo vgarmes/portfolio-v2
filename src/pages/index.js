21de63c543580b057c51cfe61cead4cd8a3c915e
Victor Garcia Mestre <vgmestre@gmail.com>
2021-05-06 00:24:30 +0200

    Projects component accepts projects as props

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,45 @@ import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
+import { graphql } from "gatsby"
 
-export default function Home() {
+const IndexPage = ({ data }) => {
+  const {
+    allStrapiProject: { nodes: projects },
+  } = data
   return (
     <Layout>
       <Hero />
       <Jobs />
-      <Projects />
+      <Projects title="featured projects" projects={projects} />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allStrapiProject(filter: { featured: { eq: true } }) {
+      nodes {
+        id
+        title
+        description
+        stack {
+          id
+          name
+        }
+        url
+        github
+        image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+            }
+          }
+        }
+        featured
+      }
+    }
+  }
+`
+
+export default IndexPage
