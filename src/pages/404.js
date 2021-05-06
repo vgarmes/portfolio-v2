ba1bdf06c66c8bd0e392363aa09c91ef00f880b7
Victor Garcia Mestre <vgmestre@gmail.com>
2021-05-06 14:02:36 +0200

    added custom error page

---
+++ src/pages/404.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Error = () => {
+  return (
+    <main className="error-page">
+      <div className="error-container">
+        <h1>oops it's a dead end</h1>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+      </div>
+    </main>
+  )
+}
+
+export default Error
