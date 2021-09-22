0fb0d154116d82b6cc8dafb242ff2a5b9dad0b7b
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-22 12:10:02 +0200

    removed unused dependencies

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,9 @@
-import React, { useState, useEffect } from "react"
-import styled, { ThemeProvider } from "styled-components"
+import React, { useEffect } from "react"
+import { ThemeProvider } from "styled-components"
 import GlobalStyle from "../styles/GlobalStyle"
 import theme from "../styles/theme"
-// import "../assets/css/main.css"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
-
-// Smooth Scrolling: https://medium.com/@chrisfitkin/how-to-smooth-scroll-links-in-gatsby-3dc445299558
-/*
-if (typeof window !== "undefined") {
-  // eslint-disable-next-line global-require
-  require("smooth-scroll")('a[href*="#"]')
-}
-*/
 
 const Layout = ({ children, location }) => {
   const isHome = location.pathname === "/"
@@ -30,17 +21,20 @@ const Layout = ({ children, location }) => {
     }
   }
 
+  // Scroll into view when url contains #section
   useEffect(() => {
-    if (location.hash) {
+    /*if (location.hash) {
       const id = location.hash.substring(1) // location.hash without the '#'
+
       setTimeout(() => {
         const el = document.getElementById(id)
+        console.log(el)
         if (el) {
           el.scrollIntoView()
           el.focus()
         }
       }, 0)
-    }
+    }*/
 
     handleExternalLinks()
   }, [])
