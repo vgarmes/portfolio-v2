110aafab5fd326fb00d9666f9f2206f49e4c7a6b
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-22 13:39:36 +0200

    added comment on ssr

--- src/utils/sr.js
+++ src/utils/sr.js
@@ -1,5 +1,6 @@
 import ScrollReveal from "scrollreveal"
 
+// during build the app is rendered on server and "window" is not available to ScrollReveal
 const isSSR = typeof window === "undefined"
 const sr = isSSR ? null : ScrollReveal()
 
