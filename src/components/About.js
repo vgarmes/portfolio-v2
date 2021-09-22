508f990cbcdf625fe7ac5543ed28b94f07ae83bf
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-22 12:06:46 +0200

    used icons for technologies list

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,18 @@ import usePrefersReducedMotion from "../hooks/usePrefersReducedMotion"
 import sr from "../utils/sr"
 import { srConfig } from "../utils"
 import Title from "./Title"
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiReact,
+  SiGatsby,
+  SiNodeDotJs,
+  SiRuby,
+  SiRails,
+  SiMongodb,
+  SiPostgresql,
+} from "react-icons/si"
 
 const StyledAboutContainer = styled.div`
   display: flex;
@@ -12,10 +24,50 @@ const StyledAboutContainer = styled.div`
 
   .about-text-container {
     width: 60%;
+    text-align: center;
   }
   .skills-list {
-    display: grid;
-    grid-template-columns: repeat(2, minmax(140px, 200px));
+    ${({ theme }) => theme.mixins.flexCenter};
+    flex-wrap: wrap;
+  }
+
+  .skills-list li {
+    position: relative;
+    font-size: var(--fz-md);
+    font-weight: 500;
+    letter-spacing: 0.1em;
+    padding: 1em;
+    color: var(--clr-grey-9);
+  }
+
+  .skills-list li span {
+    visibility: hidden;
+    width: 120px;
+    background-color: var(--clr-grey-9);
+    color: var(--clr-grey-1);
+    text-align: center;
+    border-radius: var(--radius);
+    padding: 5px 0;
+    position: absolute;
+    z-index: 1;
+    bottom: 100%;
+    left: 50%;
+    margin-left: -60px;
+  }
+
+  .skills-list li span::after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: var(--clr-grey-9) transparent transparent transparent;
+  }
+
+  .skills-list li:hover span {
+    visibility: visible;
   }
 
   .about-img-container {
@@ -51,13 +103,26 @@ const About = () => {
   const containerRef = useRef(null)
   const prefersReducedMotion = usePrefersReducedMotion()
 
+  const icons = [
+    { name: "HTML5", icon: <SiHtml5 /> },
+    { name: "CSS3", icon: <SiCss3 /> },
+    { name: "JavaScript", icon: <SiJavascript /> },
+    { name: "React", icon: <SiReact /> },
+    { name: "Gatsby", icon: <SiGatsby /> },
+    { name: "Node JS", icon: <SiNodeDotJs /> },
+    { name: "Ruby", icon: <SiRuby /> },
+    { name: "Rails", icon: <SiRails /> },
+    { name: "MongoDB", icon: <SiMongodb /> },
+    { name: "PostgreSQL", icon: <SiPostgresql /> },
+  ]
+
   useEffect(() => {
     if (prefersReducedMotion) {
       return
     }
     sr.reveal(titleRef.current, srConfig())
     sr.reveal(containerRef.current, srConfig(200))
-  }, [])
+  }, [prefersReducedMotion])
 
   return (
     <section id="about" ref={containerRef}>
@@ -66,7 +131,7 @@ const About = () => {
         <div className="about-text-container">
           <div>
             <p>
-              Hello! My name is Victor. I am a multi-disciplinar engineer whose
+              Hello! My name is Victor. I am a multi-disciplinary engineer whose
               interest in web development started a few years ago when I was
               involved in the back-end development of a monitoring app at my old
               job.
@@ -80,13 +145,15 @@ const About = () => {
             </p>
           </div>
           <ul className="skills-list">
-            <li>HTML, CSS</li>
-            <li>Javascript (ES6+)</li>
-            <li>React, Gatsby</li>
-            <li>Node.js, Express</li>
-            <li>Ruby on Rails</li>
-            <li>Python</li>
-            <li>SQL, MongoDB</li>
+            {icons.map(({ name, icon }, index) => (
+              <li>
+                {React.cloneElement(icon, {
+                  key: index,
+                  size: "1.5em",
+                })}
+                <span>{name}</span>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="about-img-container">
