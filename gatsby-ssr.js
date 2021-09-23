09f72faa08dce4142807c72ab0a54b814d6dc272
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-23 10:41:03 +0200

    moved styled-component's ThemeProvider and GlobalStyles to wrapPageElement to avoid flashing of unstyled components

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,6 +4,10 @@ const { ThemeProvider } = require("styled-components")
 const theme = require("./src/styles/theme").default
 const GlobalStyle = require("./src/styles/GlobalStyle").default
 
+/*
+This makes SSR render during a short time the site without any styles applied (flash of unstyled content):
+https://github.com/spences10/ama/issues/11
+
 exports.wrapRootElement = ({ element }) => {
   return (
     <ThemeProvider theme={theme}>
@@ -11,8 +15,13 @@ exports.wrapRootElement = ({ element }) => {
       {element}
     </ThemeProvider>
   )
-}
+}*/
 
 exports.wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Layout {...props}>{element}</Layout>{" "}
+    </ThemeProvider>
+  )
 }
