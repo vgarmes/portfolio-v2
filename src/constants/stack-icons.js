ceac830fa3e32e26a2a11a2340f5de7073d98534
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-26 23:14:39 +0200

    create stack icons constants

---
+++ src/constants/stack-icons.js
@@ -0,0 +1,28 @@
+import React from "react"
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiReact,
+  SiGatsby,
+  SiNodeDotJs,
+  SiRuby,
+  SiRails,
+  SiMongodb,
+  SiPostgresql,
+} from "react-icons/si"
+
+const data = [
+  { name: "HTML5", icon: <SiHtml5 /> },
+  { name: "CSS3", icon: <SiCss3 /> },
+  { name: "JavaScript", icon: <SiJavascript /> },
+  { name: "React", icon: <SiReact /> },
+  { name: "Gatsby", icon: <SiGatsby /> },
+  { name: "Node JS", icon: <SiNodeDotJs /> },
+  { name: "Ruby", icon: <SiRuby /> },
+  { name: "Rails", icon: <SiRails /> },
+  { name: "MongoDB", icon: <SiMongodb /> },
+  { name: "PostgreSQL", icon: <SiPostgresql /> },
+]
+
+export default data
