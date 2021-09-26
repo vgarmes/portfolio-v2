2de6eccd6e5a760ec6c39be224ab2f3c16661d65
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-26 16:13:56 +0200

    added boxShadows

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -12,6 +12,23 @@ const theme = {
     desktopM: `1200px`,
     desktopL: `1400px`,
   },
+  boxShadow: {
+    small: `
+    0.5px 1px 1px hsl(var(--shadow-color) / 0.7)
+  `,
+    medium: `
+    1px 2px 2px hsl(var(--shadow-color) / 0.333),
+    2px 4px 4px hsl(var(--shadow-color) / 0.333),
+    3px 6px 6px hsl(var(--shadow-color) / 0.333)
+  `,
+    large: `
+    1px 2px 2px hsl(var(--shadow-color) / 0.2),
+    2px 4px 4px hsl(var(--shadow-color) / 0.2),
+    4px 8px 8px hsl(var(--shadow-color) / 0.2),
+    8px 16px 16px hsl(var(--shadow-color) / 0.2),
+    16px 32px 32px hsl(var(--shadow-color) / 0.2)
+  `,
+  },
   mixins,
 }
 
