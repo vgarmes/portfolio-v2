8be124e270b5bedbf395095dedb874efca60c491
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-26 21:25:29 +0200

    use SocialLink component and changed background color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,36 @@
 import React from "react"
 import social_links from "../constants/social_links"
+import SocialLink from "./SocialLink"
 import styled from "styled-components"
 
 const StyledFooter = styled.section`
-  height: 5rem;
   text-align: center;
   display: grid;
   place-items: center;
   margin-bottom: 3rem;
+  max-width: 100%;
+  padding: 10px;
+  margin-bottom: 0;
+
+  background: linear-gradient(
+    0deg,
+    var(--color-homepage-light),
+    var(--color-homepage-dark)
+  );
 
   h4 {
     margin-top: 0.5rem;
-    color: var(--clr-white);
+    color: var(--color-gray-700);
     font-weight: normal;
     text-transform: capitalize;
   }
   h4 span {
-    color: var(--clr-primary-5);
     margin-left: 0.5em;
   }
   .social-links {
     ${({ theme }) => theme.mixins.flexBetween};
     margin-top: 3rem;
     width: 15rem;
-  }
-  .social-link {
-    font-size: 1.25rem;
-    color: var(--clr-primary-2-lighter);
-    transition: var(--transition);
-    opacity: 0.85;
-  }
-  .social-link:hover {
-    opacity: 1;
-    transform: scale(1.2, 1.2);
-    color: var(--clr-primary-2-lightest);
   }
 `
 
@@ -42,15 +39,11 @@ const Footer = () => {
     <StyledFooter>
       <div className="social-links">
         {social_links.map(link => {
-          return (
-            <a href={link.url} key={link.id} className="social-link">
-              {link.icon}
-            </a>
-          )
+          return <SocialLink key={link.id} {...link} />
         })}
       </div>
       <h4>
-        {new Date().getFullYear()}
+        &copy;{new Date().getFullYear()}
         <span>Victor Garcia Mestre</span>
       </h4>
     </StyledFooter>
