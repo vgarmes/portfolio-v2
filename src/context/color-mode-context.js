3b93f2f04cfb28d6672bb6cb485a0d4a8142d359
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-27 12:57:20 +0200

    create ColorMode Context

--- src/context/color-mode-context.js
+++ src/context/color-mode-context.js
@@ -1,17 +1,44 @@
-import React, { useState } from 'react';
-import getInitialColorMode from '../utils/get-initial-color-mode';
+import React, { useState, useEffect } from 'react';
+import { COLORS, COLOR_MODE_KEY, INITIAL_COLOR_MODE_CSS_PROP } from '../styles';
 
 export const ColorModeContext = React.createContext();
 
 export const ColorModeProvider = ({ children }) => {
-  const [colorMode, rawSetColorMode] = useState(getInitialColorMode);
+  const [colorMode, rawSetColorMode] = useState(undefined);
 
-  const setColorMode = value => {
-    rawSetColorMode(value);
+  useEffect(() => {
+    const root = window.document.documentElement;
 
-    // Persist it on update
-    window.localStorage.setItem('color-mode', value);
-  };
+    const initialColorValue = root.style.getPropertyValue(
+      INITIAL_COLOR_MODE_CSS_PROP
+    );
+
+    rawSetColorMode(initialColorValue);
+  }, []);
+
+  function setColorMode(newValue) {
+    const root = window.document.documentElement;
+
+    // 1. Update React color-mode state
+    rawSetColorMode(newValue);
+
+    // 2. Update localStorage
+    localStorage.setItem('color-mode', newValue);
+
+    // 3 Update each color
+    root.style.setProperty(
+      '--color-text',
+      newValue === 'light' ? COLORS.light.text : COLORS.dark.text
+    );
+    root.style.setProperty(
+      '--color-background',
+      newValue === 'light' ? COLORS.light.background : COLORS.dark.background
+    );
+    root.style.setProperty(
+      '--color-primary',
+      newValue === 'light' ? COLORS.light.primary : COLORS.dark.primary
+    );
+  }
 
   return (
     <ColorModeContext.Provider value={{ colorMode, setColorMode }}>
