4a2c968c26acc739a7ddd6359230b893397ba261
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-27 23:30:43 +0200

    colors iteration and memoized setColorMode function

--- src/context/color-mode-context.js
+++ src/context/color-mode-context.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { COLORS, COLOR_MODE_KEY, INITIAL_COLOR_MODE_CSS_PROP } from '../styles';
 
 export const ColorModeContext = React.createContext();
@@ -16,32 +16,31 @@ export const ColorModeProvider = ({ children }) => {
     rawSetColorMode(initialColorValue);
   }, []);
 
-  function setColorMode(newValue) {
-    const root = window.document.documentElement;
+  const contextValue = useMemo(() => {
+    function setColorMode(newValue) {
+      const root = window.document.documentElement;
 
-    // 1. Update React color-mode state
-    rawSetColorMode(newValue);
+      // Update localStorage
+      localStorage.setItem(COLOR_MODE_KEY, newValue);
 
-    // 2. Update localStorage
-    localStorage.setItem('color-mode', newValue);
+      // Update properties
+      Object.entries(COLORS).forEach(([name, colorByTheme]) => {
+        const cssVarName = `--color-${name}`;
 
-    // 3 Update each color
-    root.style.setProperty(
-      '--color-text',
-      newValue === 'light' ? COLORS.light.text : COLORS.dark.text
-    );
-    root.style.setProperty(
-      '--color-background',
-      newValue === 'light' ? COLORS.light.background : COLORS.dark.background
-    );
-    root.style.setProperty(
-      '--color-primary',
-      newValue === 'light' ? COLORS.light.primary : COLORS.dark.primary
-    );
-  }
+        root.style.setProperty(cssVarName, colorByTheme[newValue]);
+      });
+
+      // Update React color-mode state
+      rawSetColorMode(newValue);
+    }
+    return {
+      colorMode,
+      setColorMode,
+    };
+  }, [colorMode, rawSetColorMode]);
 
   return (
-    <ColorModeContext.Provider value={{ colorMode, setColorMode }}>
+    <ColorModeContext.Provider value={contextValue}>
       {children}
     </ColorModeContext.Provider>
   );
