c4bbf0d2dfb135b2a52debbed7b0a54d32bbc352
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-27 12:58:03 +0200

    wrap page element in ColorMode Provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,9 @@
-const React = require("react")
-const Layout = require("./src/components/Layout").default
-const { ThemeProvider } = require("styled-components")
-const theme = require("./src/styles/theme").default
-const GlobalStyle = require("./src/styles/GlobalStyle").default
+const React = require('react');
+const Layout = require('./src/components/Layout').default;
+const { ThemeProvider } = require('styled-components');
+const { ColorModeProvider } = require('./src/context/color-mode-context');
+const theme = require('./src/styles/theme').default;
+const GlobalStyle = require('./src/styles/GlobalStyle').default;
 
 /*
 This makes SSR render during a short time the site without any styles applied (flash of unstyled content):
@@ -20,8 +21,10 @@ exports.wrapRootElement = ({ element }) => {
 exports.wrapPageElement = ({ element, props }) => {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Layout {...props}>{element}</Layout>{" "}
+      <ColorModeProvider>
+        <GlobalStyle />
+        <Layout {...props}>{element}</Layout>{' '}
+      </ColorModeProvider>
     </ThemeProvider>
-  )
-}
+  );
+};
