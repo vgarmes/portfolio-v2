575fd3a8df09e2f6e62171e4022c0a0e70ba80f4
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-27 12:55:19 +0200

    create constants for color mode

---
+++ src/styles/index.js
@@ -0,0 +1,15 @@
+export const COLORS = {
+  light: {
+    text: 'hsl(222deg, 22%, 5%)',
+    background: 'hsl(0deg, 0%, 100%)',
+    primary: 'hsl(245deg, 100%, 60%)',
+  },
+  dark: {
+    text: 'white',
+    background: 'black',
+    primary: 'blue',
+  },
+};
+
+export const COLOR_MODE_KEY = 'color-mode';
+export const INITIAL_COLOR_MODE_CSS_PROP = '--initial-color-mode';
