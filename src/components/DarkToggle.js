9214e16b3149621161e8c6b563b265a850d12c24
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-28 15:44:19 +0200

    darktoggle logic and styled-component wrapper

--- src/components/DarkToggle.js
+++ src/components/DarkToggle.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import useDarkMode from '../hooks/useDarkMode';
+import { HiSun, HiMoon } from 'react-icons/hi';
+import styled from 'styled-components';
+
+const Toggle = styled.button`
+  ${({ theme }) => theme.mixins.iconButton};
+  opacity: 0.7;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  font-size: 1.5rem;
+  color: var(--color-text);
+  &:hover,
+  &:focus,
+  &:active {
+    opacity: 1;
+  }
+`;
 
 const DarkToggle = () => {
   const [colorMode, setColorMode] = useDarkMode();
@@ -9,16 +27,14 @@ const DarkToggle = () => {
   }
 
   return (
-    <label>
-      <input
-        type="checkbox"
-        checked={colorMode === 'dark'}
-        onChange={e => {
-          setColorMode(e.target.checked ? 'dark' : 'light');
-        }}
-      />{' '}
-      Dark
-    </label>
+    <Toggle
+      aria-label="activate dark mode"
+      onClick={() => {
+        setColorMode(colorMode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {colorMode === 'light' ? <HiSun /> : <HiMoon />}
+    </Toggle>
   );
 };
 
