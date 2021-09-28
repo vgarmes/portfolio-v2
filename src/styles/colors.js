41da0ef3b0e2ef969f38120e10c0089e12bc1df8
Victor Garcia Mestre <vgmestre@gmail.com>
2021-09-28 12:01:36 +0200

    renamed color mode constants

---
+++ src/styles/colors.js
@@ -0,0 +1,21 @@
+export const COLORS = {
+  text: {
+    light: 'hsl(222deg, 22%, 5%)',
+    dark: 'white',
+  },
+  background: {
+    light: 'hsl(0deg, 0%, 100%)',
+    dark: 'hsl(210deg, 30%, 8%)',
+  },
+  'blurred-background': {
+    light: 'hsla(0deg, 0%, 100%, 0.85)',
+    dark: 'hsla(210deg, 30%, 8%, 0.85)',
+  },
+  primary: {
+    light: 'hsl(245deg, 100%, 60%)',
+    dark: 'blue',
+  },
+};
+
+export const COLOR_MODE_KEY = 'color-mode';
+export const INITIAL_COLOR_MODE_CSS_PROP = '--initial-color-mode';
